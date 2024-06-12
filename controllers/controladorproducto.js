e7759866ad7d5eafb524ec61b5117e25a446696f
Melissabeltran <desarrollo@academicos.cesde.edu.co>
2024-06-12 17:31:43 -0500

    Proyect

---
+++ controllers/controladorproducto.js
@@ -0,0 +1,19 @@
+let url="localhost:8080/storeapi/v1/producto"
+
+let peticion={
+    method:"GET",
+    Headers:{}   
+}
+
+fetch(url,peticion,)
+.then(function(respuesta){
+    return respuesta.json()
+})
+.then(function(respuesta){
+    console.log(respuesta)
+})
+.catch(function(respuesta){
+    console.log(respuesta)
+})
+
+
